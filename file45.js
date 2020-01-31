d72cbbf02a7afd0651748e4df295cd2c75a16984
PUSHKAR <pushkarmaurya20052001@gmail.com>
2020-02-01 03:02:24 +0530

    Operator not working

---
+++ file45.js
@@ -0,0 +1,42 @@
+ 
+var text=document.querySelectorAll(".textr");
+
+var buttonr =document.querySelector(".io");
+
+buttonr.addEventListener("click",function(){
+    for(var x=0;x<=3;x++){
+    text[x].value="";}
+    
+    
+});
+
+
+
+
+var textl=document.querySelector(".textlogin");
+
+var buttonl =document.querySelector(".btnl");
+buttonl.addEventListener("click",function(){
+    
+    textl.value="You have been successfully loged in!";
+    lo
+    
+    
+})
+
+
+var btn=document.querySelector("#ghg");
+btn.addEventListener("click",function(){
+    
+    
+    location.reload("true");
+    
+})
+
+
+
+
+
+
+
+
